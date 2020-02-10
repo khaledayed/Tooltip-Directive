7dcd67a213e6b6c3df9ac4a2d00fb5edc1ca18ab
Khaled Ayed <ayed.khaled@gmail.com>
2020-02-10 23:30:41 +0100

    second commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'awesome-root',
+  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  name: string = "Hello World"
+}
